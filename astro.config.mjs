841ea683c428b3afe3c5b02f31fd72d66ef37364
Nishkal Rao <101920752+nishkalrao20@users.noreply.github.com>
2025-08-15 23:59:19 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: 'https://nishkalrao20.github.io',
   base: '/',
+  integrations: [
+    mdx({
+      /rehypePlugins: [rehypeKatex],
+    }),
   vite: {
     plugins: [tailwindcss()],
   },
 });
-
-import rehypeKatex from "rehype-katex";
-
-export default {
-  integrations: [
-    // ...
-    mdx({
-      rehypePlugins: [rehypeKatex],
-    })
-  ]
-}
