f257ac93123d14f0dce7c0e0f895f48dd9c87d14
Nishkal Rao <101920752+nishkalrao20@users.noreply.github.com>
2025-08-16 00:03:13 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://nishkalrao20.github.io',
   base: '/',
-  integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
