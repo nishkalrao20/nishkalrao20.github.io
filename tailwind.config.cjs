79638b84dc68dfcc6f4d3f0e3379e0581704c63c
Nishkal Rao <101920752+nishkalrao20@users.noreply.github.com>
2025-08-15 23:39:56 +0530

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,14 @@
 module.exports = {
+  content: ["./src/**/*.{astro,js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       fontFamily: {
-        serif: ['"Source Serif 4"', 'ui-serif', 'Georgia', 'serif'],
+        serif: ['"Source Serif 4"', 'serif'],
+        mono: ['"IBM Plex Mono"', 'monospace'],
       },
     },
   },
-};
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
