ccad309b3b361bc40bcad78007e2cf45449686ce
Nishkal Rao <101920752+nishkalrao20@users.noreply.github.com>
2025-08-16 00:24:47 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import rehypeKatex from "rehype-katex";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://nishkalrao20.github.io",
-  base: "/",
-  integrations: [
-    tailwind(),
-    mdx({
-      rehypePlugins: [rehypeKatex],
-    }),
-  ],
+  site: 'https://nishkalrao20.github.io',
+  base: '/',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
