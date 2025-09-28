8066edec8bb5824c261712fefbe1dc6492c0c000
Nishkal Rao <101920752+nishkalrao20@users.noreply.github.com>
2025-09-28 19:59:00 +0530

    Delete tailwind.config.cjs

--- tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ['./src/**/*.{astro,html,js,ts,jsx,tsx,md,mdx}'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-};
