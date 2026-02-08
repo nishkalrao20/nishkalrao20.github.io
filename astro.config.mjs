1ccd80cdd377bd55c8f77d04cc4c8e6a43b20439
Nishkal Rao <101920752+nishkalrao20@users.noreply.github.com>
2026-02-08 14:24:16 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://nishkalrao20.github.io",
@@ -15,6 +16,7 @@ export default defineConfig({
       remarkPlugins: [remarkMath],
       rehypePlugins: [rehypeKatex],
     }),
+    sitemap(),
   ],
   markdown: {
     remarkPlugins: [remarkMath],
